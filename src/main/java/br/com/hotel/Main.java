3d92656bc2698eafa538481733591564ea1b1b39
Axel <axelselbach90@gmail.com>
2026-06-14 19:35:43 -0300

    Refactor: Adjusted main.java and pom.xml

--- src/main/java/br/com/hotel/Main.java
+++ src/main/java/br/com/hotel/Main.java
@@ -1,17 +1,38 @@
 package br.com.hotel;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Carrega a tela principal (dashboard)
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/dashboard.fxml"));
+
+            Scene scene = new Scene(loader.load());
+
+            // Configurações da janela principal
+            primaryStage.setTitle("Hotel Meia Boca Juniors - Sistema de Gerenciamento");
+            primaryStage.setScene(scene);
+            primaryStage.setMinWidth(1000);
+            primaryStage.setMinHeight(650);
+            primaryStage.setResizable(true);
+
+            primaryStage.setMaximized(true);
+
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Erro ao iniciar a aplicação: " + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
